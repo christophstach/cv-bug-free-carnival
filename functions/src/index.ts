5ac3038245039771d4dfea32a3d023f24f68b1c8
Christoph Stach <christoph.stach@gmail.com>
2017-09-27 19:38:17 -0500

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 const https = require('firebase-functions').https;
-const setupServer = require('./graphql/server.js').setupServer;
+const setupServer = require('./src/graphql/server.js').setupServer;
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
