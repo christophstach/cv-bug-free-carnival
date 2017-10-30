b781de5e5654d0237b31692d0bc63c7891f79091
Christoph Stach <christoph.stach@gmail.com>
2017-10-29 23:56:51 -0600

    Angular Universal auf Firebase functions

--- src/app/shared/services/scroll-spy/scroll-spy.service.ts
+++ src/app/shared/services/scroll-spy/scroll-spy.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/observable/fromEvent';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/empty';
 
 @Injectable()
 export class ScrollSpyService {
   private windowScroll$: Observable<Event>;
 
   constructor() {
-    this.windowScroll$ = Observable.fromEvent(window, 'scroll');
-    this.windowScroll$.subscribe((data) => {
-      console.log(data);
-    });
+    if (typeof window !== 'undefined') {
+      this.windowScroll$ = Observable.fromEvent(window, 'scroll');
+    } else {
+      this.windowScroll$ = Observable.empty();
+    }
   }
 
   getObservable(): Observable<Event> {
